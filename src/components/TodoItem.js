69afdf8c1057bc94867d55b7d42117915054c1ff
Mi alter ego <fgiancarelli@gmail.com>
2018-04-22 12:12:37 -0300

    Added and styled logo as item tickmark.

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -18,7 +18,7 @@ class TodoItem extends Component {
       start: e.target.selectionStart,
       end: e.target.selectionEnd
     }
-    if(pos.start === pos.end) {
+    if (pos.start === pos.end) {
       this.setState(() => ({
         cursorPosition: pos.start
       }));
@@ -34,10 +34,10 @@ class TodoItem extends Component {
     }));
   }
 
-  handleInput(e){
+  handleInput(e) {
     e.target.style.height = 'auto';
     e.target.style.height = (e.target.scrollHeight) + 'px';
-    
+
     const target = e.target;
     const item = this.props.item;
     const newContent = target.value;
@@ -93,6 +93,7 @@ class TodoItem extends Component {
         }));
       }
       //textField.focus();
+      //✓
     }
   }
 
@@ -102,11 +103,26 @@ class TodoItem extends Component {
       <div className='TodoItem-container'>
         <div
           className={'TodoItem' + (this.state.editingMode === true ? ' editing-mode' : '')} >
-          <input
+          <button
             className={'btn-resolve' + (item.state === 1 ? ' resolved' : '')}
-            type='button'
-            onClick={() => this.props.handleAlternateState()}
-            value='✓' />
+            onClick={() => this.props.handleAlternateState()} >
+            <svg
+              className='item-tickmark'
+              xmlns="http://www.w3.org/2000/svg"
+              width="261.427"
+              height="261.427"
+              viewBox="0 0 69.16935 69.16935"
+              id="svg8" >
+              <g id="layer1" transform="translate(2884.32 1117.216)">
+                <g id="g4682" transform="translate(0 -8.646)" fill="#999" strokeWidth="0.265">
+                  <path id="path121-0-3-5-4" d="m -2836.7517,-1086.9683 -21.6291,21.6286 -17.2925,-17.2924 -8.646,8.6459 25.9385,25.939 30.271,-30.271 a 13.670831,13.670845 0 0 1 -8.6419,-8.6501 z"
+                    fillRule="evenodd" />
+                  <ellipse id="path928-40-9-64-1" cx="2823.796" cy="-1091.278" rx="8.646"
+                    ry="8.646" transform="scale(-1 1)" />
+                </g>
+              </g>
+            </svg>
+          </button>
           <textarea
             onDoubleClick={(e) => this.handleDoubleClick(e)}
             onClick={(e) => this.handleClick(e)}
